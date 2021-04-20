4906470e928854d0e89175a9687dc7d1d35d7bd2
Ronny Florian <ronniflorian@hotmail.com>
2021-04-20 11:37:33 -0400

    a

---
+++ main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class Item(BaseModel):
+    name: str
+    price: float
+
+#domain where this api is hosted for example : localhost:5000/docs to see swagger documentation automagically generated.
+
+
+@app.get("/")
+def home():
+    return {"message":"Hello TutLinks.com"}
+
+@app.post("/api")
+def postItem(item:Item):
+    return {"itemName": item.name, "itemPrice":item.price}
+
