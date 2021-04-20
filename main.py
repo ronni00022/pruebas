c92986a532f229c6cfa0c10bf62707193fad36e8
Ronny Florian <ronniflorian@hotmail.com>
2021-04-20 12:44:36 -0400

    w

--- main.py
+++ main.py
@@ -1,18 +1,10 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from fastapi.middleware.cors import CORSMiddlewar
 
 app = FastAPI()
 
 origins = ["*"]
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 class Item(BaseModel):
     name: str
